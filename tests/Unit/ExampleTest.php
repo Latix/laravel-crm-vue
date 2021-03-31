eb49c10f3d7e388ea9f6681ca8dd6e9edc98d81f
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-31 13:32:47 +0100

    Added Database For Testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,31 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
+     * A basic test example works.
      *
      * @return void
      */
     public function testBasicTest()
     {
+        $user = User::create([
+            'company_id' => 0,
+            'name' => 'Kamsi Cruise',
+            'email' => 'kk@gmail.com',
+            'account_type' => 'Admin',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10)
+        ]);
+        $this->assertCount(1, User::all());
         $this->assertTrue(true);
     }
 }
