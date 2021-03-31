b2eba8d8b2349ef6e2f09d6f20bb7f10ca39c39b
Kamsi Kodi <kamsikodi@gmail.com>
2021-03-31 13:00:09 +0100

    Code Refactored

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example works update.
+     * A basic test example works.
      *
      * @return void
      */
